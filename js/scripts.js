f0740e85bad521d09d9cab2bb037d98d93361313
tlpackus <tlpackus@gmail.com>
2021-01-29 10:43:24 -0800

    follow lesson and the last thing i added was an alert to pop up if an age is not entered

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,17 @@ $(document).ready(function() {
     const age = parseInt($("input#age").val());
     const gender = $("select#gender").val();
 
-    const quote = (100 - age) * 3;
+    if (age) {
+      let quote = (100 - age) * 3;
+      if (gender === 'male' && age < 26) {
+        quote += 50;
+      }
 
-    $("#rate").text(quote);
-    $("#quote").show();
+      $("#rate").text(quote);
+      $("#quote").show();
+    } else {
+      alert('Please enter your age.');
+    }
 
     event.preventDefault();
   });
